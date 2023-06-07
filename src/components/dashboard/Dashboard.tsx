111a100802a1fba6e2ffb1cc6df95017ab96ed7a
Matheus Lima <matheus.lima@ccc.ufcg.edu.br>
2023-06-07 16:57:16 -0300

    fix: responsividade e menu mobile

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,22 +1,86 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./dashboard.module.scss";
-import { useRouter } from "next/router";
+import { List, X } from "@phosphor-icons/react";
 
 export default function Dashboard() {
-  const router = useRouter();
+  const [isMobile, setIsMobile] = useState(false);
 
-  const handleVoltar = () => {
-    router.back();
-  };
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 1024);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className={styles.wrapper}>
+      {isMobile ? <MobileMenu /> : <DesktopMenu />}
+    </div>
+  );
+}
+
+function MobileMenu() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <>
+      <button className={styles.menuIcon} onClick={toggleMenu}>
+        {menuOpen ? (
+          <div className={styles.menuMobileTitle}>
+            <h3>Menu</h3>
+            <X size={34} color='white' />
+          </div>
+        ) : (
+          <div className={styles.menuMobileTitle}>
+            <h3>Menu</h3>
+            <List size={34} color='white' />
+          </div>
+        )}
+      </button>
+
+      {menuOpen && (
+        <ul className={styles.menu}>
+          <li>
+            <Link href={"/painel"}>Painel</Link>
+          </li>
+          <li>
+            <Link href={"/experiencia"}>Adicionar Área de Experiência</Link>
+          </li>
+          <li>
+            <Link href={"/criarProjeto"}>Criar Projeto</Link>
+          </li>
+          <li>
+            <Link href={"/meusProjetos"}>Meus Projetos</Link>
+          </li>
+          <li>
+            <Link href={"/perfil"}>Perfil</Link>
+          </li>
+        </ul>
+      )}
+    </>
+  );
+}
+
+function DesktopMenu() {
+  return (
+    <>
       <h3>Menu</h3>
-      <ul>
+      <ul className={styles.menu}>
         <li>
           <Link href={"/painel"}>Painel</Link>
         </li>
-
         <li>
           <Link href={"/experiencia"}>Adicionar Área de Experiência</Link>
         </li>
@@ -29,12 +93,7 @@ export default function Dashboard() {
         <li>
           <Link href={"/perfil"}>Perfil</Link>
         </li>
-        {/* <li>
-          <Link href={""} onClick={handleVoltar}>
-            Voltar
-          </Link>
-        </li> */}
       </ul>
-    </div>
+    </>
   );
 }
