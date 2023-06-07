de23b5e0dbc9ee22b3706e9dd58a1427000d7cbf
Matheus Lima <matheus.lima@ccc.ufcg.edu.br>
2023-06-07 17:07:52 -0300

    fix: responsividade footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,17 @@ import Footer from "@/components/footer/Footer";
 import { Provider } from "@/context/Context";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import styles from "@/styles/app.module.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider>
-      <Component {...pageProps} />
-      <Footer />
+      <div className={styles.wrapper}>
+        <Component {...pageProps} />
+        <div className={styles.footer}>
+          <Footer />
+        </div>
+      </div>
     </Provider>
   );
 }
